71ad01c3edc50677a5186a244d78d636b02b4625
Grigori Grudinin <grgr14@mail.ru>
2017-06-03 17:11:37 +0300

    add

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import fs from 'mz/fs';
+import debug from 'debug';
 import { getFolder, getPath } from './lib/pathAdapter';
 import parseHTML from './lib/parseHTML/';
 import loadAndWrite from './loadAndWrite';
+
+const flag = debug('page-loader');
 
 export default (dir, url) => {
   const folder = getFolder(dir, url);
@@ -14,7 +17,9 @@ export default (dir, url) => {
     .then(() => loadAndWrite(url, dir))
     .then(() => fs.readFile(pathFile, 'utf8'))
     .then((data) => {
+      flag(`file ${pathFile} saved`);
       [resourseUrl, newData] = parseHTML(data, folder);
+      console.log(resourseUrl);
       return fs.writeFile(pathFile, newData);
     })
     .then(() =>
